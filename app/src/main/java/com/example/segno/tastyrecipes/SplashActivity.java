b8e2114819b85654c19cdd3eeeecf081d67820bc
Jessionid <ds972869347@163.com>
2016-09-01 20:01:46 +0800

    Initial commit about the Project  base SourceCode.

---
+++ app/src/main/java/com/example/segno/tastyrecipes/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.segno.tastyrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ *  @author     Jession Ding
+ *  created at  2016/9/1 19:54
+ *  Description 打开APP的初始化动画效果
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
